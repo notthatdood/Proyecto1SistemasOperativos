73aa818908c2c9315e3b43f6b12614dbd5ed9884
notthatdood <asanchez280300@gmail.com>
2023-04-16 23:50:32 +0000

    first upload from linux

---
+++ proyecto1.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS     5
+
+void *PrintHello(void *threadid) {
+   long did;
+   did = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", did);
+   pthread_exit(NULL);
+}
+
+int main (int argh, char *argv[]) {
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0; t<NUM_THREADS; t++) {
+      printf("In main: creating thread %ld\n", t);
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      if (rc) {
+         printf("ERROR; return code from pthread_create() is %d\n", rc);
+         exit(-1);
+      }
+   }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+}
